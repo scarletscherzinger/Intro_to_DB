c7eec127a1a15ddcb43c82e7be58c3139de067bd
Scarlet Scherzinger <scarletscherzinger@gmail.com>
2025-10-19 12:11:03 +0300

    Correct task_2.sql: exact table and column names in uppercase

--- task_2.sql
+++ task_2.sql
@@ -3,38 +3,38 @@
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(215)
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
 
 CREATE TABLE Books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(215),
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders(id),
-    FOREIGN KEY (book_id) REFERENCES Books(id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
